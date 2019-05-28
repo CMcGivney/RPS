50b925ea8273e66f28861e790ead6faed53f3b5c
Chris McGivney <cmmcgivney84@gmail.com>
2019-05-28 14:25:36 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var options = ["rock", "paper", "scissors"];
+var usersChoice;
+var compChoice;
+var choice = document.getElementsByClassName("choice");
+console.log(choices);
+for(var i = 0; i < choices.length; i++) {
+  choices[i].addEventListener("click", function(){
+  userChoice = this.id
+  //get the comp choice
+  //display choices
+  //figure out winner
+});
+}
